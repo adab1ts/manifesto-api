83b6656feaa21b7a3f911aaa9b1baace88f066b6
Carles Muiños <carles.ml.dev@gmail.com>
2017-11-20 12:52:13 +0100

    Add validation rules to Signer

--- common/models/signer.js
+++ common/models/signer.js
@@ -2,4 +2,10 @@
 
 module.exports = function(Signer) {
 
+  // Validate the email is well formed
+  Signer.validatesFormatOf('email', { with: /(.+)@(.+){2,}\.(.+){2,}/ });
+
+  // Validate the email to be unique
+  Signer.validatesUniquenessOf('email');
+
 };
