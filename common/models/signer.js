30d954bf34aaa53e0595b700eeb2cffccbea4411
Carles Muiños <carles.ml.dev@gmail.com>
2017-11-20 18:16:44 +0100

    Fix standard linting errors

--- common/models/signer.js
+++ common/models/signer.js
@@ -1,19 +1,17 @@
-'use strict';
+'use strict'
 
-module.exports = function(Signer) {
-
+module.exports = function (Signer) {
   // Validate the email is well formed
-  Signer.validatesFormatOf('email', { with: /(.+)@(.+){2,}\.(.+){2,}/ });
+  Signer.validatesFormatOf('email', { with: /(.+)@(.+){2,}\.(.+){2,}/ })
 
   // Validate the email to be unique
   // TODO: Create an operation hook to check for uniqueness in PG
-  Signer.validatesUniquenessOf('email');
+  Signer.validatesUniquenessOf('email')
 
   // TODO: Set created_at property value
-  Signer.observe('before save', function(ctx, next) {
+  Signer.observe('before save', function (ctx, next) {
     if (ctx.instance) {
     }
     return next()
-  });
-
-};
+  })
+}
