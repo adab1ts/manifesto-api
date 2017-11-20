e0e07fa3059d9c1eeac9bcc964907e24321da15d
Carles Muiños <carles.ml.dev@gmail.com>
2017-11-20 13:09:09 +0100

    WIP: Add operation hooks
    TODO:
    * check uniqueness of email with PG DB in mind before saving
    * Set created_at property value before saving

--- common/models/signer.js
+++ common/models/signer.js
@@ -6,6 +6,14 @@ module.exports = function(Signer) {
   Signer.validatesFormatOf('email', { with: /(.+)@(.+){2,}\.(.+){2,}/ });
 
   // Validate the email to be unique
+  // TODO: Create an operation hook to check for uniqueness in PG
   Signer.validatesUniquenessOf('email');
+
+  // TODO: Set created_at property value
+  Signer.observe('before save', function(ctx, next) {
+    if (ctx.instance) {
+    }
+    return next()
+  });
 
 };
